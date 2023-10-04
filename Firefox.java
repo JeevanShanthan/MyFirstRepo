1e2b4d4a785978671ac73b75005a1e92e8af0bb9
JeevanShanthan <146314525+JeevanShanthan@users.noreply.github.com>
2023-10-04 09:04:13 +0530

    Selenium

--- Firefox.java
+++ Firefox.java
@@ -1,13 +1,25 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 class Firefox
 {
 	public static void main(String[] args) {
-		System.out.println("Selenium 1st program");
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Jeevan Shanthan\\eclipse-workspace\\SeleniumJavaFramework\\drivers\\geckodriver\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.meesho.com/");
-		driver.close();
+		driver.get("http://www.gmail.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("identifierId")).sendKeys("jeevanshanthan14@gmail.com");
+		driver.findElement(By.className("VfPpkd-RLmnJb")).click();
+		String at = driver.getTitle();
+		String et = "gmail";
+		if(at.equalsIgnoreCase(et))
+		{
+			System.out.println("Test is Successfull");
+		}
+		else
+		{
+			System.out.println("Test is Failure");
+		}
 	}
 }
